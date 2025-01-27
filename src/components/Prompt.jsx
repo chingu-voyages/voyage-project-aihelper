75c827850f91583c4f6d7e63c777f35804c69509
Jim <jdmedlock@gmail.com>
2025-01-27 17:56:39 -0600

    Feature: Initial project creation

--- src/components/Prompt.jsx
+++ src/components/Prompt.jsx
@@ -1,20 +1,23 @@
+import { useState } from 'react'
 import { GoogleGenerativeAI } from '@google/generative-ai'
 
-const handleSubmit = async (e) => {
-  // Prevent the browser from reloading the page
-  e.preventDefault()
-
-  // Read the form data
-  const form = e.target
-  const formData = new FormData(form)
-  const formJson = Object.fromEntries(formData.entries())
-  const genAI = new GoogleGenerativeAI(import.meta.env.GEMINI_API_KEY)
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" })
-  const result = await model.generateContent(formJson.aiPrompt)
-  console.log(result.response.text())
-}
-
 const Prompt = () => {
+  const [aiAnswer, setAiAnswer] = useState(''); // Declare a state variable...
+
+  const handleSubmit = async (e) => {
+    // Prevent the browser from reloading the page
+    e.preventDefault()
+  
+    // Read the form data
+    const form = e.target
+    const formData = new FormData(form)
+    const formJson = Object.fromEntries(formData.entries())
+    const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY)
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" })
+    const result = await model.generateContent(formJson.aiPrompt)
+    setAiAnswer(result.response.text())
+  }
+
   return (
     <div>
       <form method="post" onSubmit={ handleSubmit }>
@@ -25,7 +28,8 @@ const Prompt = () => {
         <button type="submit">Submit</button>
         <hr />        
       </form>
-      <p>Results:</p>
+      <h1>Results:</h1>
+      {aiAnswer !== '' && <p>{ aiAnswer }</p>}
       <hr />
     </div>
   )
